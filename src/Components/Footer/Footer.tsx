98cc78f04f8dfcb3207e21c51ac3a896957bf0d6
niteshchauhan <niteshchauhan1717@gmail.com>
2025-05-20 15:33:00 +0530

    Frontend Add

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -29,7 +29,7 @@ const Footer = () => {
     </div>
     <Divider/>
     <div data-aos="flip-left"  data-aos-offset="0" className="font-medium text-center p-5">
-        Designed & Developed By <a className="text-bright-sun-400 hover:underline font-semibold " href="https://github.com/Code-Mars">Saurav Singh</a>
+        Designed & Developed By <a className="text-bright-sun-400 hover:underline font-semibold " href="https://github.com/Code-Mars">Nitesh Singh</a>
     </div>
     </div>:<></>
 }
