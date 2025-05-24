72c14751ef335cd5f859bf481b06500e75c645bb
niteshchauhan <niteshchauhan1717@gmail.com>
2025-05-24 18:15:54 +0530

    Latest

--- src/Services/AuthService.tsx
+++ src/Services/AuthService.tsx
@@ -1,5 +1,5 @@
 import axiosInstance from "../Interceptor/AxiosInterceptor";
-const base_url = "http://localhost:8080/auth/"
+const base_url = "https://jobportal-backend-zwsk.onrender.com/auth/"
 const loginUser = async (login:any)=> {
     return axiosInstance.post(`${base_url}login`, login)
         .then((result:any) => result.data)
