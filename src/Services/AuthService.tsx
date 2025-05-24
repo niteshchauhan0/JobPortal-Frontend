5f6e008563448d19fcccfe9b1ec75f083023ef91
niteshchauhan <niteshchauhan1717@gmail.com>
2025-05-24 17:32:47 +0530

    Latest commit

--- src/Services/AuthService.tsx
+++ src/Services/AuthService.tsx
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axiosInstance from "../Interceptor/AxiosInterceptor";
 const base_url = "http://localhost:8080/auth/"
 const loginUser = async (login:any)=> {
-    return axios.post(`${base_url}login`, login)
+    return axiosInstance.post(`${base_url}login`, login)
         .then((result:any) => result.data)
         .catch((error:any) =>{throw error;});
 }
