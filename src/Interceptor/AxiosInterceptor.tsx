92c3c8772990014c2b2e517ac78da387fadb8a5f
niteshchauhan <niteshchauhan1717@gmail.com>
2025-05-24 17:46:11 +0530

    Latest

--- src/Interceptor/AxiosInterceptor.tsx
+++ src/Interceptor/AxiosInterceptor.tsx
@@ -3,8 +3,8 @@ import { removeUser } from "../Slices/UserSlice";
 import { removeJwt } from "../Slices/JwtSlice";
 
 const axiosInstance = axios.create({
-    // baseURL: 'http://localhost:8080'
-    baseURL: 'https://jobportal-backend-zwsk.onrender.com'
+    baseURL: 'http://localhost:8080'
+    // baseURL: 'https://jobportal-backend-zwsk.onrender.com'
 });
 
 axiosInstance.interceptors.request.use(
